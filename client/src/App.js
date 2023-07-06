fb87dd682d471ea12738cf3f029f9ece94e80c82
VMKCreeper <92611907+VMKCreeper@users.noreply.github.com>
2023-07-06 17:35:38 -0400

    creating memories and post form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid} from '@mui/material'
+import memories from './components/images/memories.png'
 
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" aligh="center">Memories</Typography>
+                <img src={memories} alt="memories"></img>
+            </AppBar>
+        </Container>
     )
 }
 
