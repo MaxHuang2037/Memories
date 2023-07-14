1d6125fc951e0b8d0b9317dd72d7bec9aec8a3fc
VMKCreeper <92611907+VMKCreeper@users.noreply.github.com>
2023-07-14 18:12:36 -0400

    removed unnecessary files

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,12 +3,11 @@ import {useSelector} from 'react-redux'
 import styles from "./styles.module.css"
 const Posts = () => {
     const {posts} = useSelector((state) => state.post)
-    console.log(posts.length)
     return(
         <section className={styles.container}>
             <h1>Posts</h1>
             {
-                (posts.length == 0) ? 
+                (posts === undefined) ? 
                 <h1>Loading...</h1> :
                 posts.map((data) => {
                     return <Post key={data._id} data={data}></Post>
