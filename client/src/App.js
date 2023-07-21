3da5df5ad0f990bb7f3f5fd71ae847e5758e4b83
VMKCreeper <92611907+VMKCreeper@users.noreply.github.com>
2023-07-21 12:21:35 -0400

    created .env files, need to fix problems with google auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import Auth from './components/Auth/Auth'
 import { Routes, Route } from 'react-router-dom'
+import {GoogleOAuthProvider} from "@react-oauth/google"
+import dotenv from "dotenv"
 
 import styles from "./styles.module.css"
 
+dotenv.config()
 const App = () => {
     return (
-        <main className={styles.main}>
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/auth" element={<Auth/>}/>
-            </Routes>
-        </main>
+        <GoogleOAuthProvider clientId={`${process.env.GOOGLE_API_TOKEN}`}>
+            <main className={styles.main}>
+                <Navbar/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/auth" element={<Auth/>}/>
+                </Routes>
+            </main>
+        </GoogleOAuthProvider>
     )
 }
 
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 import postRoutes from './routes/post.js'
 
+dotenv.config()
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
@@ -13,7 +15,7 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const URI = "mongodb+srv://user-1:gsbzss777@nodetaskmanager.7y7vy.mongodb.net/?retryWrites=true&w=majority"
+const URI = process.env.MONGO_URI
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(URI, {
